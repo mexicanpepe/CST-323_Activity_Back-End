3f19f8c79fd3799c749e9ad6ec98b75a0572c0f0
Joseph Camarena <pepecamarena5@gmail.com>
2024-02-17 17:46:44 -0700

    Initialized Backend for CRUD APP

---
+++ cst323_activity_backend/src/main/java/com/gcu/cst323_activity_backend/Cst323ActivityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.cst323_activity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst323ActivityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst323ActivityBackendApplication.class, args);
+	}
+
+}
---
+++ cst323_activity_backend/src/test/java/com/gcu/cst323_activity_backend/Cst323ActivityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.cst323_activity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst323ActivityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
